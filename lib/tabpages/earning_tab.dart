c139772a0966a4cf5187763a5d569aadbfb43835
Masp2601 <masp2601@gmail.com>
2022-10-18 10:47:27 -0500

    se agrega appBar

--- lib/tabpages/earning_tab.dart
+++ lib/tabpages/earning_tab.dart
@@ -10,8 +10,41 @@ class EarningsTabPage extends StatefulWidget {
 class _EarningsTabPageState extends State<EarningsTabPage> {
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("Pagos"),
+    return Scaffold(
+          appBar: AppBar(
+            title: const Text(
+              "Wakypet",
+              style: TextStyle(fontWeight: FontWeight.normal),
+            ),
+            titleSpacing: -16,
+            //color de fondo
+            backgroundColor: Colors.blueAccent,
+            // Establecer la imagen de la pantalla frontal
+            leading: IconButton(
+              icon: const Icon(Icons.location_on_outlined),
+              onPressed: () {},
+            ),
+            // Coloca el icono detrás
+            actions: <Widget>[
+              SizedBox.fromSize(
+                size: const Size(56, 56), // button width and height
+                child: ClipOval(
+                  child: Material(
+                    color: Colors.lightBlue, // button color
+                    child: InkWell(
+                      onTap: () {}, // button pressed
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: const <Widget>[
+                          Icon(Icons.whatsapp_sharp, color: Colors.white), // text
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
     );
   }
 }
